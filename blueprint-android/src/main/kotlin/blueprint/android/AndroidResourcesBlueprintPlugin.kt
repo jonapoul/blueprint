ba6f1d5dc4fe4baca3294f63cd6d1fd383c5679a
Jon Poulton <jpoulton@pm.me>
2024-02-26 20:37:52 +0000

    Allow resources with other plugins

--- blueprint-android/src/main/kotlin/blueprint/android/AndroidResourcesBlueprintPlugin.kt
+++ blueprint-android/src/main/kotlin/blueprint/android/AndroidResourcesBlueprintPlugin.kt
@@ -3,7 +3,6 @@
 package blueprint.android
 
 import com.android.build.gradle.LibraryExtension
-import org.gradle.api.GradleException
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
@@ -11,14 +10,6 @@ public class AndroidResourcesBlueprintPlugin : Plugin<Project> {
   override fun apply(target: Project) {
     target.pluginManager.apply("com.android.library")
     target.pluginManager.apply("dev.jonpoulton.blueprint.android.base")
-
-    val hasOtherPluginsApplied = target.plugins.any { plugin ->
-      plugin is AndroidLibraryBlueprintPlugin
-    }
-    if (hasOtherPluginsApplied) {
-      val allPlugins = target.plugins.joinToString()
-      throw GradleException("Can't use resources blueprint alongside lib or app blueprints. Plugins: $allPlugins")
-    }
 
     target.extensions.configure(LibraryExtension::class.java) { ext ->
       ext.configureExtension()
