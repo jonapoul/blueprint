afccb38974ca46399d0981549045bcbf291613ea
Jon Poulton <jpoulton@pm.me>
2024-02-26 19:27:50 +0000

    Only sign publications when env var is set

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
   alias(libs.plugins.dependencyGuard)
   alias(libs.plugins.dokka) apply false
   alias(libs.plugins.kotlin) apply false
-  alias(libs.plugins.publish) apply false
+  alias(libs.plugins.publish)
 }
 
 dependencyGuard {
@@ -16,4 +16,11 @@ dependencyGuard {
 tasks.wrapper {
   version = libs.versions.gradle.get()
   distributionType = Wrapper.DistributionType.ALL
+}
+
+mavenPublishing {
+  val releaseSigningEnv = System.getenv("ORG_GRADLE_PROJECT_RELEASE_SIGNING_ENABLED")?.toBoolean()
+  if (releaseSigningEnv != false) {
+    signAllPublications()
+  }
 }
