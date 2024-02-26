801fe00f82abfd9fe7dfcc617fc7cafd42066945
Jon Poulton <jpoulton@pm.me>
2024-02-26 18:48:57 +0000

    Lazily evaluate throwing properties

--- blueprint-atak/src/main/kotlin/blueprint/atak/AtakProperties.kt
+++ blueprint-atak/src/main/kotlin/blueprint/atak/AtakProperties.kt
@@ -6,7 +6,7 @@ import org.gradle.api.Project
 internal class AtakProperties(override val project: Project) : BlueprintProperties() {
   override val keyPrefix = "blueprint.atak"
 
-  val pluginVersion = stringPropertyOrThrow(key = "version.plugin")
+  val pluginVersion by lazy { stringPropertyOrThrow(key = "version.plugin") }
   val atakVersion = stringProperty(key = "version.atak", default = BuildConfig.ATAK_BASE_VERSION)
   val sdkVersion = stringProperty(key = "version.sdk", default = BuildConfig.ATAK_SDK_VERSION)
   val sdkCoordinates = stringProperty(key = "sdkCoordinates", default = "com.atakmap.civ.common:api")
@@ -17,5 +17,5 @@ internal class AtakProperties(override val project: Project) : BlueprintProperti
   val proguardRules = stringProperty(key = "proguardFile.rules", default = "proguard-rules.pro")
   val proguardMapping = stringProperty(key = "proguardFile.rules", default = "proguard-mapping.pro")
 
-  val apkName = stringPropertyOrThrow(key = "apkName")
+  val apkName by lazy { stringPropertyOrThrow(key = "apkName") }
 }
