0a61df84254b0b0f43485c543cafdf634d05814e
Jon Poulton <jpoulton@pm.me>
2024-07-20 20:26:55 +0100

    Optionally add testing libs

--- blueprint-recipes/src/main/kotlin/blueprint/recipes/Test.kt
+++ blueprint-recipes/src/main/kotlin/blueprint/recipes/Test.kt
@@ -3,9 +3,11 @@ package blueprint.recipes
 import com.android.build.gradle.AppExtension
 import com.android.build.gradle.LibraryExtension
 import org.gradle.api.Project
+import org.gradle.api.artifacts.Configuration
 import org.gradle.api.provider.Provider
 import org.gradle.api.tasks.testing.Test
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.kotlin.dsl.DependencyHandlerScope
 import org.gradle.kotlin.dsl.dependencies
 import org.gradle.kotlin.dsl.findByType
 import org.gradle.kotlin.dsl.withType
@@ -21,22 +23,22 @@ public fun Project.testBlueprint(
   val testImplementation = configurations.findByName("testImplementation")
   dependencies {
     testImplementation?.let { testImplementation ->
-      testImplementation("dev.jonpoulton.alakazam:testing-core:${versions.alakazam.get()}")
-      testImplementation("junit:junit:${versions.junit.get()}")
-      testImplementation("org.jetbrains.kotlin:kotlin-test-common:${versions.kotlin.get()}")
-      testImplementation("org.jetbrains.kotlin:kotlin-test-junit:${versions.kotlin.get()}")
-      testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:${versions.coroutines.get()}")
-      testImplementation("io.mockk:mockk:${versions.mockk.get()}")
-      testImplementation("app.cash.turbine:turbine:${versions.turbine.get()}")
+      add(testImplementation, "dev.jonpoulton.alakazam:testing-core", versions.alakazam)
+      add(testImplementation, "junit:junit", versions.junit)
+      add(testImplementation, "org.jetbrains.kotlin:kotlin-test-common", versions.kotlin)
+      add(testImplementation, "org.jetbrains.kotlin:kotlin-test-junit", versions.kotlin)
+      add(testImplementation, "org.jetbrains.kotlinx:kotlinx-coroutines-test", versions.coroutines)
+      add(testImplementation, "io.mockk:mockk", versions.mockk)
+      add(testImplementation, "app.cash.turbine:turbine", versions.turbine)
       otherKotlinTestLibs.forEach { testImplementation(it) }
 
       if (isAndroid) {
-        testImplementation("androidx.arch.core:core-testing:${versions.androidxArch.get()}")
-        testImplementation("androidx.test:core-ktx:${versions.androidxCoreKtx.get()}")
-        testImplementation("androidx.test.ext:junit:${versions.androidxJunit.get()}")
-        testImplementation("androidx.test:rules:${versions.androidxRules.get()}")
-        testImplementation("androidx.test:runner:${versions.androidxRunner.get()}")
-        testImplementation("org.robolectric:robolectric:${versions.robolectric.get()}")
+        add(testImplementation, "androidx.arch.core:core-testing", versions.androidxArch)
+        add(testImplementation, "androidx.test:core-ktx", versions.androidxCoreKtx)
+        add(testImplementation, "androidx.test.ext:junit", versions.androidxJunit)
+        add(testImplementation, "androidx.test:rules", versions.androidxRules)
+        add(testImplementation, "androidx.test:runner", versions.androidxRunner)
+        add(testImplementation, "org.robolectric:robolectric", versions.robolectric)
         otherAndroidTestLibs.forEach { testImplementation(it) }
       }
     }
@@ -54,16 +56,21 @@ public fun Project.testBlueprint(
 }
 
 public data class TestVersions(
-  val alakazam: Provider<String>,
-  val junit: Provider<String>,
-  val kotlin: Provider<String>,
-  val coroutines: Provider<String>,
-  val mockk: Provider<String>,
-  val turbine: Provider<String>,
-  val androidxArch: Provider<String>,
-  val androidxCoreKtx: Provider<String>,
-  val androidxJunit: Provider<String>,
-  val androidxRules: Provider<String>,
-  val androidxRunner: Provider<String>,
-  val robolectric: Provider<String>,
+  val alakazam: Provider<String>?,
+  val junit: Provider<String>?,
+  val kotlin: Provider<String>?,
+  val coroutines: Provider<String>?,
+  val mockk: Provider<String>?,
+  val turbine: Provider<String>?,
+  val androidxArch: Provider<String>?,
+  val androidxCoreKtx: Provider<String>?,
+  val androidxJunit: Provider<String>?,
+  val androidxRules: Provider<String>?,
+  val androidxRunner: Provider<String>?,
+  val robolectric: Provider<String>?,
 )
+
+private fun DependencyHandlerScope.add(config: Configuration, lib: String, version: Provider<String>?) {
+  version ?: return
+  config("$lib:${version.get()}")
+}
