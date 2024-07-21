2a4a2c6b89fdc2e1de23323a76bc4707732a56c5
Jon Poulton <jpoulton@pm.me>
2024-07-21 10:39:26 +0100

    Dont auto-apply kotlin for android libs

--- blueprint-recipes/src/main/kotlin/blueprint/recipes/AndroidLib.kt
+++ blueprint-recipes/src/main/kotlin/blueprint/recipes/AndroidLib.kt
@@ -4,20 +4,13 @@ package blueprint.recipes
 
 import com.android.build.api.dsl.LibraryExtension
 import org.gradle.api.Project
-import org.gradle.api.provider.Provider
 import org.gradle.kotlin.dsl.getByType
 
-public fun Project.androidLibBlueprint(
-  kotlinVersion: Provider<String>,
-  explicitApi: Boolean = true,
-  freeCompilerArgs: List<String> = DEFAULT_KOTLIN_FREE_COMPILER_ARGS,
-) {
+public fun Project.androidLibBlueprint() {
   with(plugins) {
-    apply("org.jetbrains.kotlin.android")
     apply("com.android.library")
   }
 
-  kotlinBlueprint(kotlinVersion, explicitApi, freeCompilerArgs)
   androidBaseBlueprint()
 
   extensions.getByType(LibraryExtension::class).apply {
