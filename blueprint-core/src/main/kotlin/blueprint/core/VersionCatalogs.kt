96a168f0311ebd519d13b1fec33d788cc5fa6153
Jon Poulton <jpoulton@pm.me>
2025-03-08 19:16:22 +0000

    Create Libs.kt

--- blueprint-core/src/main/kotlin/blueprint/core/VersionCatalogs.kt
+++ blueprint-core/src/main/kotlin/blueprint/core/VersionCatalogs.kt
@@ -1,9 +1,15 @@
 package blueprint.core
 
+import org.gradle.api.Project
 import org.gradle.api.artifacts.MinimalExternalModuleDependency
 import org.gradle.api.artifacts.VersionCatalog
+import org.gradle.api.artifacts.VersionCatalogsExtension
 import org.gradle.api.artifacts.VersionConstraint
 import org.gradle.api.provider.Provider
+import org.gradle.kotlin.dsl.getByType
+
+public val Project.libs: VersionCatalog
+  get() = extensions.getByType<VersionCatalogsExtension>().named("libs")
 
 public fun VersionCatalog.getLibrary(alias: String): Provider<MinimalExternalModuleDependency> {
   return findLibrary(alias).get()
