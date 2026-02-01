9980535971caa249956453a1574b725f1d0707bb
Jon Poulton <jpoulton@pm.me>
2026-02-01 09:19:44 +0000

    detektReportMergeSarif

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,16 @@
+import io.gitlab.arturbosch.detekt.Detekt
+import io.gitlab.arturbosch.detekt.report.ReportMergeTask
+
 plugins {
   alias(libs.plugins.buildConfig) apply false
-  alias(libs.plugins.detekt) apply false
   alias(libs.plugins.dokka) apply false
   alias(libs.plugins.kotlin) apply false
   alias(libs.plugins.kotlinAbi) apply false
   alias(libs.plugins.publish) apply false
+
   alias(libs.plugins.dependencyAnalysis)
   alias(libs.plugins.dependencyGuard)
+  alias(libs.plugins.detekt)
   alias(libs.plugins.publishReport)
 }
 
@@ -19,5 +23,17 @@ dependencyAnalysis {
     all {
       onAny { severity("fail") }
     }
+  }
+}
+
+val detektReportMergeSarif by tasks.registering(ReportMergeTask::class) {
+  output = layout.buildDirectory.file("reports/detekt/merge.sarif.json")
+}
+
+tasks.named("check").configure { dependsOn(detektReportMergeSarif) }
+
+allprojects {
+  detektReportMergeSarif.configure {
+    input.from(tasks.withType<Detekt>().map { it.reports.sarif.outputLocation })
   }
 }
