51d65f6a33b3b1897d4eed0b82de1ac1f9f14015
Jon Poulton <jpoulton@pm.me>
2026-02-14 19:27:00 +0000

    Use Detekt's `allRules` (#132)

--- blueprint-core/src/main/kotlin/blueprint/core/BlueprintPlugin.kt
+++ blueprint-core/src/main/kotlin/blueprint/core/BlueprintPlugin.kt
@@ -3,8 +3,9 @@ package blueprint.core
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
+/**
+ * No-op, only used to bring the other utilities into the classpath.
+ */
 public class BlueprintPlugin : Plugin<Project> {
-  override fun apply(target: Project) {
-    // no-op, just brings the others into the classpath
-  }
+  override fun apply(target: Project): Unit = Unit
 }
