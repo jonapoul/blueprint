f6ebd60c94c96efacf1933f61ed371aa36aa5a6f
Jon Poulton <jpoulton@pm.me>
2026-06-26 19:49:05 +0100

    Ktfmt 0.64

--- blueprint-core/src/main/kotlin/blueprint/core/LocalProperties.kt
+++ blueprint-core/src/main/kotlin/blueprint/core/LocalProperties.kt
@@ -34,8 +34,9 @@ public fun Settings.localProperties(
     }
   }
 
-public fun Provider<Map<String, String>>.getOptional(key: String): String? =
-  map { props -> props[key] }.orNull?.takeIf { it.isNotEmpty() }
+public fun Provider<Map<String, String>>.getOptional(key: String): String? = map { props ->
+  props[key]
+}.orNull?.takeIf { it.isNotEmpty() }
 
 private abstract class LocalPropertiesValueSource :
   ValueSource<Map<String, String>, LocalPropertiesValueSource.Parameters> {
