bc1c0d77006b4b57f6ecea3f8bfc675cff4503a2
Jon Poulton <jpoulton@pm.me>
2026-09-20 14:47:52 +0100

    Redundant things

--- gradle/repositories.gradle.kts
+++ gradle/repositories.gradle.kts
@@ -1,21 +1,13 @@
 pluginManagement {
   repositories {
     mavenCentral()
-    google()
     gradlePluginPortal()
-    maven("https://central.sonatype.com/repository/maven-snapshots/") {
-      content { includeGroup("dev.detekt") }
-    }
   }
 }
 
 dependencyResolutionManagement {
   repositories {
     mavenCentral()
-    google()
     gradlePluginPortal()
-    maven("https://central.sonatype.com/repository/maven-snapshots/") {
-      content { includeGroup("dev.detekt") }
-    }
   }
 }
